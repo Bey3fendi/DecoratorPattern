97aacf26877d42a8661172bfc6d6d9446e27e170
FURKAN <furkanozdemir199998@gmail.com>
2025-07-13 12:10:12 +0300

    Add factory for more proper object creation

---
+++ DecoratorPattern/DecoratorFactory.h
@@ -0,0 +1,66 @@
+﻿#pragma once
+#include "DecoratorPattern.h"
+
+const enum StackNotification {
+	kEmailNotify,
+	kFacebookNotify,
+	kSMSNotify,
+	kSMSandEmailNotify,
+	kFacebookandEmailNotify,
+	kAllOfThemTogether
+};
+
+
+class DecoratorFactory {
+public:
+	std::shared_ptr<INotifier> create(StackNotification sn) {
+        std::shared_ptr<INotifier> in;
+
+        if (sn == kEmailNotify) {
+            // Plain email notifier
+            in = std::make_shared<Notifier>();
+
+        }
+        else if (sn == kFacebookNotify) {
+            // Facebook decorator around the base notifier
+            auto base = std::make_shared<Notifier>();
+            in = std::make_shared<FaceBookNotifyDecorator>(base);
+
+        }
+        else if (sn == kSMSNotify) {
+            // SMS decorator around the base notifier
+            auto base = std::make_shared<Notifier>();
+            in = std::make_shared<SMSNotifyDecorator>(base);
+
+        }
+        else if (sn == kSMSandEmailNotify) {
+            // First wrap base in a generic decorator, then SMS
+            auto base = std::make_shared<Notifier>();
+            auto decorated = std::make_shared<BaseDecorator>(base);
+            in = std::make_shared<SMSNotifyDecorator>(decorated);
+
+        }
+        else if (sn == kFacebookandEmailNotify) {
+            // First wrap base in a generic decorator, then Facebook
+            auto base = std::make_shared<Notifier>();
+            auto decorated = std::make_shared<BaseDecorator>(base);
+            in = std::make_shared<FaceBookNotifyDecorator>(decorated);
+
+        }
+        else if (sn == kAllOfThemTogether) {
+            // Base → generic decorator → Facebook → SMS
+            auto base = std::make_shared<Notifier>();
+            auto dec1 = std::make_shared<BaseDecorator>(base);
+            auto dec2 = std::make_shared<FaceBookNotifyDecorator>(dec1);
+            in = std::make_shared<SMSNotifyDecorator>(dec2);
+
+        }
+        else {
+            // (Optional) handle unknown sn values
+            throw std::invalid_argument("Unknown notification type");
+        }
+
+
+		return in;
+	}
+};
--- DecoratorPattern/DecoratorPattern.cpp
+++ DecoratorPattern/DecoratorPattern.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include "DecoratorPattern.h"
+#include "DecoratorFactory.h"
 int main() {
 	//std::shared_ptr<INotifier> in = std::make_shared<Notifier>();
 	//std::shared_ptr<BaseDecorator> bd = std::make_shared<BaseDecorator>(in);
 	//std::shared_ptr<FaceBookNotifyDecorator> fnd = std::make_shared<FaceBookNotifyDecorator>(bd);
 	//std::shared_ptr<SMSNotifyDecorator> snd = std::make_shared<SMSNotifyDecorator>(fnd);
 
-	auto in = std::make_shared<Notifier>();
-	auto bd = std::make_shared<BaseDecorator>(in);
-	auto fnd = std::make_shared<FaceBookNotifyDecorator>(bd);
-	auto snd = std::make_shared<SMSNotifyDecorator>(fnd);
-	ClientCode(*snd);
+	//auto in = std::make_shared<Notifier>();
+	//auto bd = std::make_shared<BaseDecorator>(in);
+	//auto fnd = std::make_shared<FaceBookNotifyDecorator>(bd);
+	//auto snd = std::make_shared<SMSNotifyDecorator>(fnd);
+	auto fctry = std::make_shared<DecoratorFactory>();
+	auto object = fctry->create(StackNotification::kAllOfThemTogether);
+	auto object2 = fctry->create(StackNotification::kFacebookNotify);
+
+	ClientCode(*object);
+	ClientCode(*object2);
+
 }
 
